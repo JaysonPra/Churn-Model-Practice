36a8b311f6dcd573326a69296bef1f33dbf29100
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-01-12 21:24:58 +0545

    API takes raw features

--- src/main.py
+++ src/main.py
@@ -3,47 +3,61 @@ import mlflow
 from mlflow.tracking import MlflowClient
 import mlflow.pyfunc
 import fastapi
+from src.components.preprocessing import preprocess_data
 from pydantic import BaseModel, Field
 from typing import Literal
 import pandas as pd
 
 class CustomerData(BaseModel):
-    tenure: int = Field(..., ge=0, description="Months the customer has stayed")
+    tenure: int = Field(..., ge=0)
+    MonthlyCharges: float = Field(..., ge=0)
     TotalCharges: float = Field(..., ge=0)
-    TotalServices: int = Field(..., ge=0, description="Count of active services")
-    Monthly_Per_Service: float = Field(..., ge=0)
 
-    SeniorCitizen: Literal["Yes", "No"] 
+    gender: Literal["Male", "Female"]
+    SeniorCitizen: int = Field(..., description="0 or 1")
+    Partner: Literal["Yes", "No"]
     Dependents: Literal["Yes", "No"]
     PhoneService: Literal["Yes", "No"]
+    MultipleLines: Literal["Yes", "No", "No phone service"]
     InternetService: Literal["DSL", "Fiber optic", "No"]
     OnlineSecurity: Literal["Yes", "No", "No internet service"]
     OnlineBackup: Literal["Yes", "No", "No internet service"]
     DeviceProtection: Literal["Yes", "No", "No internet service"]
     TechSupport: Literal["Yes", "No", "No internet service"]
+    StreamingTV: Literal["Yes", "No", "No internet service"]
+    StreamingMovies: Literal["Yes", "No", "No internet service"]
     Contract: Literal["Month-to-month", "One year", "Two year"]
     PaperlessBilling: Literal["Yes", "No"]
-    PaymentMethod: Literal["Electronic check", "Mailed check", "Bank Transfer (automatic)", "Credit card (automatic)"]
+    PaymentMethod: Literal[
+        "Electronic check", 
+        "Mailed check", 
+        "Bank transfer (automatic)", 
+        "Credit card (automatic)"
+    ]
 
     model_config = {
         "extra": "ignore",
         "json_schema_extra": {
             "example": {
-                "tenure": 12,
-                "TotalCharges": 800.5,
-                "TotalServices": 3,
-                "Monthly_Per_Service": 25.0,
-                "SeniorCitizen": "No",
-                "Dependents": "Yes",
-                "PhoneService": "Yes",
-                "InternetService": "Fiber optic",
+                "gender": "Female",
+                "SeniorCitizen": 0,
+                "Partner": "Yes",
+                "Dependents": "No",
+                "tenure": 1,
+                "PhoneService": "No",
+                "MultipleLines": "No phone service",
+                "InternetService": "DSL",
                 "OnlineSecurity": "No",
                 "OnlineBackup": "Yes",
                 "DeviceProtection": "No",
                 "TechSupport": "No",
+                "StreamingTV": "No",
+                "StreamingMovies": "No",
                 "Contract": "Month-to-month",
                 "PaperlessBilling": "Yes",
-                "PaymentMethod": "Electronic check"
+                "PaymentMethod": "Electronic check",
+                "MonthlyCharges": 29.85,
+                "TotalCharges": 29.85
             }
         }
     }
@@ -91,13 +105,17 @@ def predict(data: CustomerData):
     if model is None:
         return {"error": "Model not loaded. Use /manage/promote first."}
 
-    input_df = pd.DataFrame([data.model_dump()])
+    full_df = pd.DataFrame([data.model_dump()])
+    full_df = preprocess_data(full_df)
 
-    for col in input_df.columns:
-        if input_df[col].dtype == 'int64':
-            input_df[col] = input_df[col].astype('int32')
-        elif input_df[col].dtype == 'float64':
-            input_df[col] = input_df[col].astype('float32')
+    try:
+        required_columns = model.metadata.get_input_schema().input_names()
+        filtered_df = full_df[required_columns]
+    except Exception as e:
+        filtered_df = full_df
 
-    prediction = model.predict(input_df)
+    for col in filtered_df.select_dtypes(['category']).columns:
+        filtered_df[col] = filtered_df[col].astype(str)
+
+    prediction = model.predict(filtered_df)
     return {"churn_prediction": int(prediction[0])}
